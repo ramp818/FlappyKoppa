f28dcc4108244fd343de19e84a3c7d1b44ae4bd5
Ruben Pacheco <pachecaso9436@gmail.com>
2014-03-10 11:46:14 -0600

    agregaron clases
    agregararon algunas clases

---
+++ Examen2/src/examen2/Examen2.java
@@ -0,0 +1,16 @@
+package examen2;
+
+/**
+ *
+ * @author Ruben Martinez y Angel Gonzalez 
+ */
+public class Examen2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
